ab7336c4e6cd6aa78c6b9a3eb6dbdde3bb280782
Andy Dzemski <andreas.dzemski@gmail.com>
2018-10-03 11:24:26 +0200

    add license notes in all source files

--- R/define_model.R
+++ R/define_model.R
@@ -1,3 +1,22 @@
+## This R package implements the methods proposed in 
+## Dzemski, Andreas: An empirical model of dyadic link formation in 
+## a network with unobserved heterogeneity, Review of Economics and Statistics, forthcoming
+
+## Copyright (C) 2018  Andreas Dzemski
+
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #' Define a network model
 #'
 #' @param d data.frame with link data
